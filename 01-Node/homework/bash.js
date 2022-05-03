7c7aff410b8d8ec8d5d057b168e72f2ecaa6c323
AntonioMelini <antoniomelini@hotmail.com>
2022-05-03 14:14:11 -0300

    homework-01

---
+++ 01-Node/homework/bash.js
@@ -0,0 +1,24 @@
+const commands = require('./commands/index.js');
+
+const done=function(output){
+  process.stdout.write(output);
+  process.stdout.write("\nprompt > ");
+}
+
+//const cmd = 'pwd';
+
+// Output un prompt
+process.stdout.write('prompt > ');
+// El evento stdin 'data' se dispara cuando el user escribe una línea
+process.stdin.on('data', function (data) {
+  var args = data.toString().trim().split(" ");
+  var cmd = args.shift(); // remueve la nueva línea
+  if(commands[cmd] ) {
+   commands[cmd](args,done);
+  }
+  else{
+    process.stdout.write(`${cmd} not found`)
+  }
+ 
+});
+
--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -1,5 +1,56 @@
-
+var fs = require('fs');
+const request=require("request")
 
 module.exports = {
-
+    date: function(arg,done){
+        done(Date());  
+    },
+    pwd:function(arg,done){
+        done(process.cwd())
+    },
+    ls:function(arg,done){
+        fs.readdir('.', function(err, files) {
+            if (err) throw err;
+            let strfiles="";
+            files.forEach(function(files){
+            strfiles=strfiles+files+"\n"
+            
+          });
+          done(strfiles)
+        })
+    },
+    echo:function(arg,done){
+        done(arg.join(" "))
+    },
+    cat: function(arg,done){
+        fs.readFile(arg[0],function(err,data){
+            if(err) throw err;
+            done(data);
+        })
+    },
+    head: function(arg,done){
+        fs.readFile(arg[0], "utf-8",function(err,data){
+            if(err) throw err;
+            const firstline= data.split("\n").slice(0,10).join("\n");
+            done(firstline)
+            
+        })
+    },
+    tail: function(arg,done){
+        fs.readFile(arg[0], "utf-8",function(err,data){
+            if(err) throw err;
+            const endlines= data.split("\n").slice(-10).join("\n");
+            done(endlines)
+            
+        })
+    },
+    curl: function(arg,done){
+        request(arg[0], function(err,response,body){
+            if(err) throw err;
+            done(body)
+            
+        })
+    }
+    
+    
 }
