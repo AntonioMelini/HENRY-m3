baaba2b0ef49bde71cbde0be33006c93e130e428
AntonioMelini <antoniomelini@hotmail.com>
2022-05-05 15:54:41 -0300

    first-part homework-04

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -4,9 +4,28 @@ Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
 
+  
+function $Promise(executor){
+    if(typeof executor != "function")  throw TypeError("executor its not a function")
+    this._state="pending"
+    this._value=undefined
+    executor(this._internalResolve.bind(this),this._internalReject.bind(this))
+   
+}
+$Promise.prototype._internalResolve=function(data){
+    if(this._state==="pending"){
+        this._state="fulfilled";
+        this._value=data
+    }
+}
+$Promise.prototype._internalReject=function(reason){
+    if(this._state==="pending"){
+        this._state="rejected"
+        this._value=reason;
+    }
+}
 
-
-module.exports = $Promise;
+//module.exports = $Promise;
 /*-------------------------------------------------------
 El spec fue diseñado para funcionar con Test'Em, por lo tanto no necesitamos
 realmente usar module.exports. Pero aquí está para referencia:
