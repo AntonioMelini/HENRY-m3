20a5c3b1ad64498e8f1c9a7a27280734adfcf197
AntonioMelini <antoniomelini@hotmail.com>
2022-05-12 12:02:39 -0300

    homework-07

--- 07-AsyncAwait/Generators/homework/homework.js
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -1,5 +1,46 @@
-function fizzBuzzGenerator(max) {
+
+
+function* fizzBuzzGenerator(max) {
   // Tu código acá:
+  let i=1;
+  if(!max){
+    while(true){
+      if(i%5===0 && i%3===0) {
+        yield "Fizz Buzz";
+        i++
+      }
+      else if(i%3===0) {
+        yield "Fizz";
+        i++
+      }
+      else if(i%5===0) {
+        yield "Buzz";
+        i++
+      }
+      else{ yield i
+      i ++;
+      }
+    }
+  }
+  else{
+    while(i<=max){
+      if(i%3 === 0 && i%5 === 0) {
+        yield "Fizz Buzz"
+        i++
+      }
+      else if(i % 3 === 0) {
+        yield "Fizz";
+        i++
+      }
+      else if(i % 5 === 0) {
+        yield "Buzz";
+        i++
+      }
+      else {yield i
+        i ++;
+      }
+    }
+  }
 
 }
 
